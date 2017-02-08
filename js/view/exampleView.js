7db52efa59f8da3e9b7e577dcb9fee0c3eaeb0f7
zedofin <zedo.fin@gmail.com>
2017-02-08 16:34:56 +0100

    asdasdasd

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,12 +1,16 @@
 //ExampleView Object constructor
-var ExampleView = function (container) {
+var ExampleView = function (container, model) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	this.numberOfGuests = container.find("#numberOfGuests");
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
-	
-	this.numberOfGuests.html("Hello World");
+
+	 this.numberOfGuests = container.find("#numberOfGuests");
+	 this.plusButton = container.find("#plusGuest");
+	 this.minusButton = container.find("#minusGuest");
+	 
+	 this.numberOfGuests.html(model.getNumberOfGuests());
+	 //$("#total").html("asdadadsa"); 
+
+	 //getTotalMenuPrice()
 }
  

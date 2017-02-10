276e9e00267a2519206ea2eb6d0e5b48312a897d
zedofin <zedo.fin@gmail.com>
2017-02-10 01:14:24 +0100

    viewit päivitetty

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -20,6 +20,9 @@ var ExampleView = function (container, model) {
 	 this.dishImage = container.find("#dishImage");
 	 this.dishPreparation = container.find("#dishPreparation");
 	 this.dishIngredients = container.find("#dishIngredients");
+	 // View 3
+	 this.searchResults = container.find("#searchResults");
+
 	 // View 5
 	 this.myDinnerXPeople = container.find("#myDinnerXPeople");
 	 this.starterImage = container.find("#starterImage");
@@ -32,6 +35,8 @@ var ExampleView = function (container, model) {
 	 this.starterPrice5 = container.find("#starterPrice5");
 	 this.mainPrice5 = container.find("#mainPrice5");
 	 this.dessertPrice5 = container.find("#dessertPrice5");
+	 // View 6
+	 this.fullRecipe = container.find("#fullRecipe");
 	 
 	 // get things from model and put in html
 	 this.starter.html(model.getDish(model.getSelectedDish("starter")).name);
@@ -47,12 +52,11 @@ var ExampleView = function (container, model) {
 	 this.dishPreparation.html(model.getDish(chosenDish).description);
 	 
 	 var ings = model.getDish(chosenDish).ingredients;
-	 var dishIngredientsHtml = "<table>";
+	 var dishIngredientsHtml = "<table id=\"ingredientsTable\">";
 	 for (i in ings){
 	 	dishIngredientsHtml += "<tr><td>" + ings[i].quantity +" "+ ings[i].unit +"</td><td>"+ ings[i].name + "</td><td> SEK </td><td> " + ings[i].price +"</td></tr>";
 	 }
 	 dishIngredientsHtml += "</table>";
-
 	 this.dishIngredients.html(dishIngredientsHtml);
 
 	 this.plusButton = container.find("#plusGuest");
@@ -63,6 +67,16 @@ var ExampleView = function (container, model) {
 		    $("#totalCost").html($("#numberOfGuests").val()*model.getTotalMenuPrice());
 		    $("#dishPeople").html("Ingredients for " + $("#numberOfGuests").val() + " people");
 		});
+
+	// View 3
+	 var results = model.getFullMenu();
+	 var searchResultsHtml = "<table id=\"searchResultsOuterTable\"><tr>";
+	 for (i in results){
+	 	searchResultsHtml += "<td><table id=\"searchResultsInnerTable\"><tr><td><img id=\"selectThisDish\" onClick=\"selectThisDish()\" src=\"images/" + results[i].image +"\" /></td></tr><tr><td>" + results[i].name +"</td></tr><tr><td>" + results[i].description +"</td></tr></table></td>";
+	 }
+	 searchResultsHtml += "</tr></table>";
+	 this.searchResults.html(searchResultsHtml);
+
 
 	// View 5
 	this.myDinnerXPeople.html("My Dinner: " + this.numberOfGuests.attr("value"));
@@ -80,6 +94,16 @@ var ExampleView = function (container, model) {
 	this.dessertImage.attr("src","images/"+model.getDish(model.getSelectedDish("dessert")).image);
 
 	this.totalPrice.html(model.getTotalMenuPrice() + " SEK");
+
+
+	// View 6
+	 var menu = model.getFullMenu();
+	 var fullRecipeHtml = "<table>";
+	 for (i in menu){
+	 	fullRecipeHtml += "<tr><td><img src=\"images/" + menu[i].image +"\" /></td><td><h2>"+ menu[i].name +"</h2><p>"+ menu[i].description + "</p></td><td><h3>Preparation</h3><p>" + menu[i].description +"</p></td></tr>";
+	 }
+	 dishIngredientsHtml += "</table>";
+	 this.fullRecipe.html(fullRecipeHtml);
 
 
 	 

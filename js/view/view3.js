959c0b987892b802a97db0a0bb3140cb5591bc2f
Paeron <Paeron@users.noreply.github.com>
2017-02-15 10:54:59 +0100

    Add files via upload

--- js/view/view3.js
+++ js/view/view3.js
@@ -9,8 +9,8 @@ var View3 = function (container, model) {
 
 	// View 3
 	// creating search results
-	 //var results = model.getAllDishes(this.dishType.text(), this.dishKeyword);
 	 var results = model.getAllDishes("starter");
+	 //var results = model.getAllDishes("starter");
 
 	 var searchResultsHtml = '<table id="searchResultsOuterTable"><tr>';
 	 for (i in results){
@@ -26,20 +26,34 @@ var View3 = function (container, model) {
 
 	 this.searchDishes.click(function(){
 	 	this.searchResults = container.find("#searchResults");
-	 	results = model.getAllDishes(type, keyword);
+	 	this.dishType = document.getElementById("dishType");
+	 	this.dishKeyword = container.find("#dishKeyword");
+	 	//console.log(this.dishType.options.selectedIndex);
+	 	//console.log(this.dishType.options[this.dishType.options.selectedIndex]);
+	 	if(this.dishKeyword.val() == "") {results = model.getAllDishes(this.dishType.options[this.dishType.options.selectedIndex].value);}
+	 	else {results = model.getAllDishes(this.dishType.options[this.dishType.options.selectedIndex].value,this.dishKeyword.val());}
 
  		 var searchResultsHtml = '<table id="searchResultsOuterTable"><tr>';
 		 for (i in results){
-		 	searchResultsHtml += '<td><table id="searchResultsInnerTable"><tr><td><img onClick="" id="selectThisDish" number="'+ results[i].name +'" src="images/' + results[i].image +'" /></td></tr><tr><td>' + results[i].name + '</td></tr><tr><td>' + results[i].description + '</td></tr></table></td>';
+		 	searchResultsHtml += '<td><table id="searchResultsInnerTable"><tr><td><img onClick="" id="'+ results[i].id +'" src="images/' + results[i].image +'" /></td></tr><tr><td>' + results[i].name + '</td></tr><tr><td>' + results[i].description + '</td></tr></table></td>';
 		 }
 		 searchResultsHtml += '</tr></table>';
 		 this.searchResults.html(searchResultsHtml);
+		 this.selectThisDish = container.find("img");
+		 	 	this.selectThisDish.click(function(e){
+	 	  //model.setShowDish(event.target.number);
+	 	  model.setShowDish(e.target.id);
+		  document.getElementById("view1").style.display = "none";
+		  document.getElementById("view3").style.display = "none";
+		  document.getElementById("view4").style.display = "block";
+		  document.getElementById("view5").style.display = "none";
+		  document.getElementById("view6").style.display = "none";
+		  showSideBar();
+	 	});
 	 });
 
-
-
-	 $( document ).ready(function() {
-	 	this.selectThisDish = container.find("#selectThisDish");
+	 
+	 	this.selectThisDish = container.find("img");
 
 	 	this.selectThisDish.click(function(e){
 	 	  //model.setShowDish(event.target.number);
@@ -52,7 +66,7 @@ var View3 = function (container, model) {
 		  document.getElementById("view6").style.display = "none";
 		  showSideBar();
 	 	});
-	 });
+	 
 
 
 }
--- js/view/view4.js
+++ js/view/view4.js
@@ -1,6 +1,6 @@
 var View4 = function (container, model) {	
 
-	var chosenDish = 100;
+	var chosenDish = model.getShowDish();
 
  	 this.dishPeople = container.find("#dishPeople");
  	 this.dishName = container.find("#dishName");
@@ -35,6 +35,18 @@ var View4 = function (container, model) {
 	 // Ingredients: Amount of people
 	 this.dishPeople.innerHtml = model.getNumberOfGuests();
 
+	 var chosenDish = model.getShowDish();
+	 this.dishName.html(model.getDish(chosenDish).name);
+	 this.dishImage.attr("src","images/"+model.getDish(chosenDish).image);
+	 this.dishImage.attr("id",model.getDish(chosenDish).id);
+	 this.dishPreparation.html(model.getDish(chosenDish).description);
+	 	 var ings = model.getDish(chosenDish).ingredients;
+	 var dishIngredientsHtml = "<table id=\"ingredientsTable\">";
+	 for (i in ings){
+	 	dishIngredientsHtml += "<tr><td>" + ings[i].quantity +" "+ ings[i].unit +"</td><td>"+ ings[i].name + "</td><td> SEK </td><td> " + ings[i].price +"</td></tr>";
+	 }
+	 dishIngredientsHtml += "</table>";
+	 this.dishIngredients.html(dishIngredientsHtml);
 
 	 	
 	 }
@@ -54,8 +66,7 @@ var View4 = function (container, model) {
 		  document.getElementById("view5").style.display = "none";
 		  document.getElementById("view6").style.display = "none";
 		  showSideBar();
-
-		  model.addDishToMenu(chosenDish);
+		  model.addDishToMenu(model.getShowDish());
 	 });
 
 }

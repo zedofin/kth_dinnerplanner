fd130d801d1ea902bc80e824d0d6a3eeeacf8dfa
zedofin <zedo.fin@gmail.com>
2017-03-02 15:07:22 +0100

    blurred images in view3 search

--- js/view/view3Controller.js
+++ js/view/view3Controller.js
@@ -11,8 +11,11 @@ var View3Controller = function(view, model, container ) {
 	 		var newLineCounter = 1;
 			for (i in results){
 				results[i].description = "Lorem ipsum description right now";
-				searchResultsHtml += '<td><table id="searchResultsInnerTable"><tr><td><img onClick="" id="'+ results[i].id +'" class="dishImage" src="https://spoonacular.com/recipeImages/' + results[i].image +'" /></td></tr><tr><td><b>' + results[i].title + '</b></td></tr><tr><td>' + results[i].description + '</td></tr></table></td>';
-				//searchResultsHtml += '<td><table id="searchResultsInnerTable"><tr><td>    <div class="placeholder"> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur20"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur15"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur10"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur5"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur2"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image" ></div> </div>  </td></tr><tr><td><b>' + results[i].title + '</b></td></tr><tr><td>' + results[i].description + '</td></tr></table></td>';
+				//NORMAL IMAGES
+				//searchResultsHtml += '<td><table id="searchResultsInnerTable"><tr><td><img onClick="" id="'+ results[i].id +'" class="dishImage" src="https://spoonacular.com/recipeImages/' + results[i].image +'" /></td></tr><tr><td><b>' + results[i].title + '</b></td></tr><tr><td>' + results[i].description + '</td></tr></table></td>';
+				
+				//BLURRED IMAGES
+				searchResultsHtml += '<td><table id="searchResultsInnerTable"><tr><td>    <div class="placeholder" id="'+ results[i].id +'"> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur20"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur15"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur10"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur5"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image-blur2"></div> <div onClick="" id="'+ results[i].id +'" style="background-image:url(https://spoonacular.com/recipeImages/' + results[i].image +');" class="dishImage bg-image" ></div> </div>  </td></tr><tr><td><b>' + results[i].title + '</b></td></tr><tr><td>' + results[i].description + '</td></tr></table></td>';
 				if (newLineCounter > 1) {
 					searchResultsHtml += '</tr><tr>';
 					newLineCounter = 0;
@@ -23,7 +26,7 @@ var View3Controller = function(view, model, container ) {
 			view.searchResults.html(searchResultsHtml);
 			view.selectViewDish = container.find("imgsd");
 
-			$("[class=dishImage]").click(function(e){
+			$("div[class=placeholder]").click(function(e){
 		 		model.setShowDish(e.target.id, container.find("#dishType").val());
 			  	document.getElementById("view1").style.display = "none";
 			  	document.getElementById("view3").style.display = "none";
